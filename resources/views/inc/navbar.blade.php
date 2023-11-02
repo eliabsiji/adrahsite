20f7a90eb4592360aae9050b3c0d14ea53d5dd47
Eliab <Eliabsiji@gmail.com>
2023-11-02 20:46:54 +0100

    Update navbar.blade.php

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -36,8 +36,23 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="project.html">Portfolio</a></li>
-
+                <li class="nav-item"><a class="nav-link" href="project.html"></a></li>
+                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Portfolio<i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="pricing.html" class="dropdown-item"><span>Pricing</span></a></li>
+                        <li><a href="team.html" class="dropdown-item"><span>Team</span></a></li>
+                        <li><a href="team-details.html" class="dropdown-item"><span>Team Details</span></a></li>
+                        <li><a href="faqs.html" class="dropdown-item"><span>Faq</span></a></li>
+                        <li><a href="project-page.html" class="dropdown-item"><span>Project Page</span></a></li>
+                        <li><a href="post.html" class="dropdown-item"><span>Post Page</span></a></li>
+                        <li class="dropdown-submenu dropdown"> <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" href="#"><span>Other Pages <i class="fa fa-angle-right"></i></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="404.html" class="dropdown-item"><span>404 Page</span></a></li>
+                                <li><a href="coming-soon.html" class="dropdown-item"><span>Coming Soon</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
                 <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
             </ul>
         </div>
