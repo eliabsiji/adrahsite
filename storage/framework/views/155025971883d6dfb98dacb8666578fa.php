5e605ee669dd8e3c148d31a6283031e6f213b2e2
Eliab <Eliabsiji@gmail.com>
2023-11-02 21:23:04 +0100

    gallary

--- storage/framework/views/155025971883d6dfb98dacb8666578fa.php
+++ storage/framework/views/155025971883d6dfb98dacb8666578fa.php
@@ -20,49 +20,25 @@
                         <li><a href="video.html" class="dropdown-item"><span>Video Gallery</span></a></li>
                     </ul>
                 </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Pages <i class="fa fa-angle-down"></i></a>
+
+                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Portfolio <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="pricing.html" class="dropdown-item"><span>Weddings</span></a></li>
+                        <li><a href="team.html" class="dropdown-item"><span>Events</span></a></li>
+                        <li><a href="team-details.html" class="dropdown-item"><span>Portraits</span></a></li>
+
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">More <i class="fa fa-angle-down"></i></a>
                     <ul class="dropdown-menu">
                         <li><a href="pricing.html" class="dropdown-item"><span>Pricing</span></a></li>
-                        <li><a href="team.html" class="dropdown-item"><span>Team</span></a></li>
-                        <li><a href="team-details.html" class="dropdown-item"><span>Team Details</span></a></li>
                         <li><a href="faqs.html" class="dropdown-item"><span>Faq</span></a></li>
-                        <li><a href="project-page.html" class="dropdown-item"><span>Project Page</span></a></li>
-                        <li><a href="post.html" class="dropdown-item"><span>Post Page</span></a></li>
-                        <li class="dropdown-submenu dropdown"> <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" href="#"><span>Other Pages <i class="fa fa-angle-right"></i></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="404.html" class="dropdown-item"><span>404 Page</span></a></li>
-                                <li><a href="coming-soon.html" class="dropdown-item"><span>Coming Soon</span></a></li>
-                            </ul>
-                        </li>
                     </ul>
                 </li>
-
-                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
             </ul>
         </div>
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Gallery <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="gallery.html" class="dropdown-item"><span>Image Gallery</span></a></li>
-                        <li><a href="video.html" class="dropdown-item"><span>Video Gallery</span></a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Portfolio<i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="pricing.html" class="dropdown-item"><span>Wedding</span></a></li>
-                        <li><a href="team.html" class="dropdown-item"><span>Event</span></a></li>
-                        <li><a href="team-details.html" class="dropdown-item"><span>Portrait</span></a></li>
 
-                    </ul>
-                </li>
-
-                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-            </ul>
-        </div>
     </div>
 </nav>
 <?php /**PATH C:\xampp\htdocs\adrahsite\resources\views/inc/navbar.blade.php ENDPATH**/ ?>
