1a1e424fc962fadadbcfdb001488cf0e9e0a3123
Eliab <Eliabsiji@gmail.com>
2023-11-06 12:39:50 +0100

    working on  doyin and dapo

--- storage/framework/views/5e38e61aa49a7b6f036ceb21af4978e4.php
+++ storage/framework/views/5e38e61aa49a7b6f036ceb21af4978e4.php
@@ -15,13 +15,52 @@
     <div class="row mb-4">
         <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
             <figure>
-                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/1.jpg')); ?>" data-caption="Quisque in felis"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/1.jpg')); ?>" data-caption="Quisque in felis"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/1.jpg')); ?>" alt="" /> </a>
             </figure>
             <figure>
-                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/11d.jpg')); ?>" data-caption="Duis viverra moss"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/11d.jpg')); ?>" data-caption="Duis viverra moss"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/11d.jpg')); ?>" alt="" /> </a>
             </figure>
             <figure>
-                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/12.jpg')); ?>" data-caption="Quisque cursus place"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/12.jpg')); ?>" data-caption="Quisque cursus place"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/12.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/13.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/13.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/14.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/14.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/15.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/15.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/888.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/888.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/66.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/66.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/16.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/16.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/17.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/17.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/555.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/555.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/77.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/77.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/88.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/88.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/18.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/18.jpg')); ?>" alt="" /> </a>
             </figure>
         </div>
         <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
@@ -29,13 +68,81 @@
                 <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
             </figure>
             <figure>
-                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/999.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/999.jpg')); ?>" alt="" /> </a>
             </figure>
             <figure>
-                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/777.jpg')); ?>" alt="" /> </a>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/222.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/222.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/111.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/111.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/1s.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/1s.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapowedding/99.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/99.jpg')); ?>" alt="" /> </a>
             </figure>
         </div>
     </div>
+
+
+    <div class="row mb-4">
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/2.jpg')); ?>" data-caption="Quisque in felis"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/2.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/3.jpg')); ?>" data-caption="Duis viverra moss"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/3.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/4.jpg')); ?>" data-caption="Quisque cursus place"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/4.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/5.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/5.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/6.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/6.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/7.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/7.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/8.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/8.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/9.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/9.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/11.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/11.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/22.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/22.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/33.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/33.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+        <div class="col-6 col-md-6 animate-box" data-animate-effect="fadeInU">
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/44.jpg')); ?>" data-caption="Aliquam non luctus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/44.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/88.jpg')); ?>" data-caption="Vivamus sit amet"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapowedding/88.jpg')); ?>" alt="" /> </a>
+            </figure>
+            <figure>
+                <a class="d-block mb-4" data-fancybox="images" href="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/55.jpg')); ?>" data-caption="Nulla volutpat rhoncus"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/doyinanddapoprewedding/55.jpg')); ?>" alt="" /> </a>
+            </figure>
+        </div>
+
+    </div>
+
+
 </div>
 <?php $__env->stopSection(); ?>
 
