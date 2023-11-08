cd4dd2904b04897d98f81b031c41e9ee076a19d7
Eliab <Eliabsiji@gmail.com>
2023-11-08 20:08:06 +0100

    Update team1-details.blade.php

--- resources/views/team1-details.blade.php
+++ resources/views/team1-details.blade.php
@@ -7,12 +7,23 @@
         <div class="col-md-7 mb-30 animate-box" data-animate-effect="fadeInUp">
             <div class="img mb-60"> <img src="{{ asset('html/images/slider/2.jpg') }}" class="img-fluid" alt=""> </div>
             <h4>Adetunji</h4>
-            <p class="mb-30">I am professional photographer based on New York, creating dreamscapes with black, white and shades in-between. I love to pause the wild, happy and real moments of life, just to hear their stories untold. Viverra tristique usto duis vitae diam neque nivamus estan.</p>
+            <p class="mb-30">
+                Adetunji is a seasoned professional photographer whose passion
+                for capturing the beauty of his subjects through the lens has
+                transformed into a remarkable career. With a keen eye for detail
+                 and an innate talent for composing striking images, Adetunji has
+                  established himself as a prominent figure in the world of photography.
+                   His works showcase a diverse range of subjects,
+                   from breathtaking portraits to captivating wedding photography.
+                   Adetunji is a sought-after photographer for both personal and commercial projects.
+                   His dedication to perfecting his craft and commitment to delivering stunning,
+                 timeless photographs sets Adetunji apart as a true professional in the world of photography.
+            </p>
             <ul class="list-unstyled team-box-list mb-60">
                 <li>
                     <div class="team-box-list-icon"> <span class="fa fa-check"></span> </div>
                     <div class="team-box-list-text">
-                        <p>Over 10 years of experience</p>
+                        <p>Over 9+ years of experience</p>
                     </div>
                 </li>
                 <li>
@@ -28,9 +39,9 @@
 
             </ul>
             <div class="tab-content mb-60" id="myTabContent">
-                <div class="tab-pane fade show active" id="biography" role="tabpanel" aria-labelledby="vision-tab">
+                {{-- <div class="tab-pane fade show active" id="biography" role="tabpanel" aria-labelledby="vision-tab">
                     <p>Biography utate ons amet ravida haretra nuam the duru miss uctus the drana accumsan justo aliquam sit amet auctor orci done vitaerisus duise nisan sapien silver sapien.</p>
-                </div>
+                </div> --}}
 
             </div>
         </div>
@@ -38,7 +49,9 @@
             <div class="wrap">
                 <div class="desc">
                     <div class="section-title mb-15">Contact Me</div>
-                    <p>I am professional photographer based on New York, creating dreamscapes with black, white and shades in-between.</p>
+                    <p> Adetunji is a seasoned professional photographer whose passion
+                        for capturing the beauty of his subjects through the lens has
+                        transformed into a remarkable career.</p>
                 </div>
                 <div class="cont">
                     <div class="coll">
