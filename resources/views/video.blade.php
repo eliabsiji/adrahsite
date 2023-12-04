dded789b1e8acc146db0e1ba777a171c88004102
Eliab <Eliabsiji@gmail.com>
2023-12-04 14:18:02 +0100

    working

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -10,10 +10,10 @@
                         </div>
                         <!-- IMPORTANT: Some functionality (ajax, iframes, etc) will not work when you're opening local file directly on your browser, the code must be running on a web server. SOURCE: https://fancyapps.com/fancybox/3/docs/ -->
                         <div class="col-md-6 mb-40 animate-box" data-animate-effect="fadeInUp">
-                            <a data-fancybox="" href="https://www.youtube.com/watch?v=ybso7mzV2hY"> <img class="img-fluid" src="images/gallery/08.jpg" alt="">
+                            <a data-fancybox="" href="https://www.youtube.com/watch?v=O_77RClvyxU"> <img class="img-fluid" src="{{ asset('html/images/gallery/oliviaandjudah/12.jpg') }}" alt="">
                                 <p><span class="fa fa-play"></span></p>
                             </a>
-                            <h5>Youtube Video (Popup)</h5>
+                            <h5>Wedding</h5>
                         </div>
 
                     </div>
--- storage/framework/views/ffa034b21fb918862ceb8e3d7eacfea3.php
+++ storage/framework/views/ffa034b21fb918862ceb8e3d7eacfea3.php
@@ -9,17 +9,12 @@
                         </div>
                         <!-- IMPORTANT: Some functionality (ajax, iframes, etc) will not work when you're opening local file directly on your browser, the code must be running on a web server. SOURCE: https://fancyapps.com/fancybox/3/docs/ -->
                         <div class="col-md-6 mb-40 animate-box" data-animate-effect="fadeInUp">
-                            <a data-fancybox="" href="https://www.youtube.com/watch?v=O_77RClvyxU"> <img class="img-fluid" src="https://www.youtube.com/watch?v=O_77RClvyxU" alt="">
+                            <a data-fancybox="" href="https://www.youtube.com/watch?v=ybso7mzV2hY"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/oliviaandjudah/12.jpg')); ?>" alt="">
                                 <p><span class="fa fa-play"></span></p>
                             </a>
-                            <h5></h5>
+                            <h5>Youtube Video (Popup)</h5>
                         </div>
 
-                        <div class="col-md-6 mb-40 animate-box" data-animate-effect="fadeInUp">
-                            <iframe width="420" height="315"
-                            src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
                     </div>
                 </div>
             </div>
