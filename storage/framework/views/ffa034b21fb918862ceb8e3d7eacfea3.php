18e233bd824b386eaae0a2b6f212da375c5d33e4
Eliab <Eliabsiji@gmail.com>
2023-12-05 08:32:03 +0100

    still working

--- storage/framework/views/ffa034b21fb918862ceb8e3d7eacfea3.php
+++ storage/framework/views/ffa034b21fb918862ceb8e3d7eacfea3.php
@@ -9,10 +9,10 @@
                         </div>
                         <!-- IMPORTANT: Some functionality (ajax, iframes, etc) will not work when you're opening local file directly on your browser, the code must be running on a web server. SOURCE: https://fancyapps.com/fancybox/3/docs/ -->
                         <div class="col-md-6 mb-40 animate-box" data-animate-effect="fadeInUp">
-                            <a data-fancybox="" href="https://www.youtube.com/watch?v=ybso7mzV2hY"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/oliviaandjudah/12.jpg')); ?>" alt="">
+                            <a data-fancybox="" href="https://www.youtube.com/watch?v=O_77RClvyxU"> <img class="img-fluid" src="<?php echo e(asset('html/images/gallery/oliviaandjudah/12.jpg')); ?>" alt="">
                                 <p><span class="fa fa-play"></span></p>
                             </a>
-                            <h5>Youtube Video (Popup)</h5>
+                            <h5>Wedding</h5>
                         </div>
 
                     </div>
